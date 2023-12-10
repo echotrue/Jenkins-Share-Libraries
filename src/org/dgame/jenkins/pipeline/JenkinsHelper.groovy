347ff7700605eb7bc315a00d105d01866ce8959c
axlrose <axlrose.huang@gmail.com>
2023-12-10 19:34:10 +0800

    fixed

--- src/org/dgame/jenkins/pipeline/JenkinsHelper.groovy
+++ src/org/dgame/jenkins/pipeline/JenkinsHelper.groovy
@@ -2,13 +2,15 @@ package org.dgame.jenkins.pipeline
 
 final class JenkinsHelper {
     private final script
+    private final Logger logger
 
     JenkinsHelper(final script) {
         this.script = Objects.requireNonNull(script)
+        logger = new Logger(this.script)
     }
 
-    static void say() {
-        println("this is function say of class JenkinsHelper")
+    void say() {
+        this.logger.log("this is class JenkinsHelper function say")
     }
 }
 
